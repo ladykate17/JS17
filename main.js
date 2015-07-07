76800ebb8a48cb61e6cd9a2d9f423fba1abe7f77
ladykate17 <stamm.katherine@gmail.com>
2015-07-07 17:08:47 -0600

    add Arch + func declr

--- main.js
+++ main.js
@@ -1,24 +1,68 @@
 $(document).on('ready', function() {
 
-var plate = function(name, price, description){
-	this.name  		 = name;
-	this.price 		 = price;
-	this.description = description;
-} 
-
-var enchiladas  = new plate('Enchiladas', 5.00, 'cheesy deliciousness'); 
-var tacos       = new plate('Tacos Carnitas', 6.00, 'steak grilled with pico de gallo');
-var fajitas     = new plate('Fajitas', 7.00, 'grilled meat with peppers');
-
-$('.Menu').append('<div class="plate"><img src="http://assets.kraftfoods.com/recipe_images/Creamy-Chicken-Enchiladas-1901.jpg"><h2>'+ enchiladas.name +' </h2> '+ ' $' + enchiladas.price + '<p class="description">' + enchiladas.description +' </p><button>ORDER NOW!</button></div>')
-$('.Menu').append('<div class="plate"><img src="http://assets.kraftfoods.com/recipe_images/Grilled-Pastor-Style-Tacos-53260.jpg"><h2>'+ tacos.name +' </h2> '+ ' $' + tacos.price + '<p class="description">' + tacos.description +' </p><button>ORDER NOW!</button></div>')
-$('.Menu').append('<div class="plate"><img src="http://assets.kraftfoods.com/recipe_images/opendeploy/58256_MXM_%20K58256V0_OR1_CR_640x428.jpg" height="203" width="306"><h2>'+ fajitas.name +' </h2> '+ ' $' + fajitas.price + '<p class="description">' + fajitas.description +' </p><button>ORDER NOW!</button></div>')
-
-$('button').on('click', function(){
-	confirm('ARE YOU SURE?????????') 
-	return 
 
 
-})
+	// creates each plate
+	var plate = function(name, price, description, img, id){
+		this.name  		 = name;
+		this.price 		 = price;
+		this.description = description;
+		this.img 		 = img;
+		this.id 		 = id;
+	} 
+
+	var enchiladas  = new plate('Enchiladas', 5.00, 'cheesy deliciousness', 'http://assets.kraftfoods.com/recipe_images/Creamy-Chicken-Enchiladas-1901.jpg', 1); 
+	var tacos       = new plate('Tacos Carnitas', 6.00, 'steak grilled with pico de gallo', 'http://assets.kraftfoods.com/recipe_images/Grilled-Pastor-Style-Tacos-53260.jpg', 2);
+	var fajitas     = new plate('Fajitas', 7.00, 'grilled meat with peppers', 'http://assets.kraftfoods.com/recipe_images/opendeploy/58256_MXM_%20K58256V0_OR1_CR_640x428.jpg', 3);
+	
+
+
+
+	// creates the overall menu
+	var Menu = function(plates) {
+		this.plates = plates; // ['enchiladas', 'tacos', 'fajitas']
+	}
+	var overallMenu = new Menu(enchiladas, tacos, fajitas);
+
+
+
+
+
+	// menu.prototype.create = function() {
+
+	// 	console.log(this.name + ' ' + this.price);
+	// 	// var cart = $('<div class="item"></div>');
+	// 	// cart.text(this.name + '' + this.price)
+
+	// 	// this.el = cart
+
+	// 	// return this.el
+	// }
+
+
+	$('.container').append('<div class="menu"><h2>Menu</h2>' + Menu.toString() + '</div>');
+	// $('.Menu').append('<div class="plate" data-id="2"><img src="' + tacos.img + '"><h2>'+ tacos.name +' </h2> '+ ' $' + tacos.price + '<p class="description">' + tacos.description +' </p><button class="tacos">ORDER NOW!</button></div>')
+	// $('.Menu').append('<div class="plate" data-id="3"><img src="' + fajitas.img + '"><h2>'+ fajitas.name +' </h2> '+ ' $' + fajitas.price + '<p class="description">' + fajitas.description +' </p><button class="fajitas">ORDER NOW!</button></div>')
+
+	$('.plate').on('click', function(){
+
+		console.log($(this).attr('data-id'));
+
+		console.log( enchiladas.create())
+		// console.log( tacos.create());
+		// var s = confirm('Are you sure?') 
+		// if (s === true) {
+		// 	$('.order').append( enchiladas.create())
+		})
+
+
+
+
+		// console.log($(this).parent().attr('data-id'))
+
+
+		// 	// return $('.order').append()
+
+		// }
 
 });
